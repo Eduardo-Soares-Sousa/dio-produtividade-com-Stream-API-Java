ca846b78edcfdd953b44b3b2a1adc66aa9056993
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-03 16:27:08 -0300

    Resolução de exercícios sobre Stream API

--- StreamAPIJava/src/DesafioStreamAPI/Desafio2.java
+++ StreamAPIJava/src/DesafioStreamAPI/Desafio2.java
@@ -1,2 +1,18 @@
-package DesafioStreamAPI;public class Desafio2 {
+package DesafioStreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/*
+*       Desafio 2 - Imprima a soma dos números pares da lista:
+*   Utilizando a Stream API, realize a soma dos números pares da lista e exiba o resultado no console.
+ */
+public class Desafio2 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        int numerosPares = numeros.stream().filter(n -> n % 2 == 0).mapToInt(Integer::intValue).sum();
+        System.out.println(numerosPares);
+    }
 }
